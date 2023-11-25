d017d4d434ca34880c8d81c2bd4650b85340c191
moomooj <wndud36395@gmail.com>
2023-11-25 21:21:54 +0900

    Readonly

---
+++ src/easy/Readonly.ts
@@ -0,0 +1,12 @@
+type TupleToObject<T extends readonly PropertyKey[]> = {
+  [key in T[number]]: T;
+};
+
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+const tupleNumber = [1, 2, 3, 4] as const;
+const sym1 = Symbol(1);
+const sym2 = Symbol(2);
+const tupleSymbol = [sym1, sym2] as const;
+const tupleMix = [1, "2", 3, "4", sym1] as const;
+
+type result = TupleToObject<typeof tuple>;
